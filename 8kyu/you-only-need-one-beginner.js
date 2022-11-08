64696d153afb0617d222cada0edeab13c55ada12
Daniel Holm <dannidona3@gmail.com>
2022-11-08 09:46:17 +0100

    blaah

---
+++ 8kyu/you-only-need-one-beginner.js
@@ -0,0 +1,13 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+
+// Solution1
+// Just use the includes method. it checks if array icludes the given parameter
+function check1(a, x) {
+    return a.includes(x)
+}
+
