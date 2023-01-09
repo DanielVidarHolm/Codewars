66039816beac56ec93fa70f9d3d66b9739939535
Daniel Vidar Holm <dannidona3@gmail.com>
2023-01-09 23:12:10 +0100

    jhg

--- 7kyu/friend-or-foe.js
+++ 7kyu/friend-or-foe.js
@@ -2,7 +2,7 @@
 
 // If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
 
-// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+// Ex: Input = ["Ryan", "Kieran", k"Jason", "Yous"], Output = ["Ryan", "Yous"]
 
 // i.e.
 
@@ -10,6 +10,10 @@
 // Note: keep the original order of the names in the output.
 
 function friend(friends){
+    let filtered = friends.filter(friend => friend.length == 4)
+    return filtered
+  }
+  function friend(friends){
     let filtered = friends.filter(friend => friend.length == 4)
     return filtered
   }
